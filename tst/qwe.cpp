7e0e4972d469c89988563214dfeff38bc37386c2
osimonevin <osimonevin@gmail.com>
2020-08-17 22:50:28 +0300

    test working

---
+++ tst/qwe.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include "Formula.h"
+
+TEST(blaTest, test1) {
+    //arrange
+    //act
+    //assert
+    EXPECT_EQ (Formula::bla (0),  0);
+    EXPECT_EQ (Formula::bla (10), 20);
+    EXPECT_EQ (Formula::bla (50), 100);
+
+}
+
+TEST(AnotherBlaTest, crya) {
+    //arrange
+    //act
+    //assert
+    EXPECT_EQ (Formula::bla (0),  0);
+    EXPECT_EQ (Formula::bla (10), 20);
+    EXPECT_EQ (Formula::bla (500), 1000);
+
+}
+
